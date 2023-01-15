d85db918c08ed8cf0ce46782fa5597c963a520bd
Christopher Broome <chris.a.broome@gmail.com>
2023-01-14 19:21:04 -0500

    Move functionality to a class

--- scratch/index.js
+++ scratch/index.js
@@ -1,5 +1,6 @@
-const MAX_OFFSET = 15;
+const MAX_OFFSET = 20;
 
+/*
 function copyToCanvas(image) {
   const can = document.createElement("canvas");
   can.width = image.naturalWidth || image.width;
@@ -48,14 +49,16 @@ function layerImagesInCanvas(image, RGB, canvas, offset) {
   const ctx = canvas.ctx;
 
   ctx.clearRect(0, 0, canvas.height, canvas.width);
-  ctx.drawImage(RGB.red, -offset, -offset);
+
+  ctx.drawImage(RGB.red, 0, 0);
   ctx.globalCompositeOperation = "lighter";
-  ctx.drawImage(RGB.green, offset, offset);
-  ctx.drawImage(RGB.blue, 0, 0);
+  ctx.drawImage(RGB.green, offset, 0);
+  ctx.drawImage(RGB.blue, -offset, 0);
   ctx.globalCompositeOperation = "destination-in";
   ctx.drawImage(image, 0, 0);
   ctx.globalCompositeOperation = "source-over";
 }
+*/
 
 document.addEventListener("DOMContentLoaded", () => {
   let images = document.querySelector("img");
@@ -65,6 +68,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   for (const image of images) {
+    /*
     const RGB = separatedRGB(image);
     const canvas = createCanvas(RGB.red.width, RGB.red.height);
     document.body.appendChild(canvas);
@@ -72,7 +76,7 @@ document.addEventListener("DOMContentLoaded", () => {
     layerImagesInCanvas(image, RGB, canvas, 0);
 
     canvas.addEventListener("mouseover", (event) => {
-      layerImagesInCanvas(image, RGB, canvas, 20);
+      layerImagesInCanvas(image, RGB, canvas, 0);
       let offset = 0;
       const interval = setInterval(() => {
         if (offset > MAX_OFFSET) {
@@ -86,4 +90,121 @@ document.addEventListener("DOMContentLoaded", () => {
       layerImagesInCanvas(image, RGB, canvas, 0);
     });
   }
+  */
+    new SplitImage(image);
+  }
 });
+
+const channels = {
+  red: "#F00",
+  green: "#0F0",
+  blue: "#00F",
+};
+class SplitImage {
+  currentOffset = 0;
+  image;
+  canvas;
+  RGB;
+  currentInterval = 0;
+  currentOperation;
+
+  constructor(image) {
+    this.image = image;
+    this.RGB = this.separatedRGB(this.image);
+    this.canvas = this.createCanvas(this.RGB.red.width, this.RGB.red.height);
+    document.body.appendChild(this.canvas);
+
+    this.layerImagesInCanvas(this.image, this.RGB, this.canvas, 0);
+
+    this.canvas.addEventListener("mouseover", () => {
+      const operation = "mouseover";
+      if (this.currentOperation != operation) {
+        clearInterval(this.currentInterval);
+        this.currentOperation = operation;
+      }
+      //this.layerImagesInCanvas(this.image, this.RGB, this.canvas, 0);
+      this.currentInterval = setInterval(() => {
+        if (this.currentOffset > MAX_OFFSET) {
+          clearInterval(this.currentInterval);
+        }
+        this.layerImagesInCanvas(
+          this.image,
+          this.RGB,
+          this.canvas,
+          this.currentOffset++
+        );
+      }, 100);
+    });
+
+    this.canvas.addEventListener("mouseout", () => {
+      const operation = "mouseout";
+      if (this.currentOperation != operation) {
+        clearInterval(this.currentInterval);
+        this.currentOperation = operation;
+      }
+      this.currentInterval = setInterval(() => {
+        if (this.currentOffset <= 0) {
+          clearInterval(this.currentInterval);
+        }
+        this.layerImagesInCanvas(
+          this.image,
+          this.RGB,
+          this.canvas,
+          this.currentOffset--
+        );
+      }, 100);
+    });
+  }
+
+  createCanvas(w, h) {
+    const can = document.createElement("canvas");
+    can.width = w;
+    can.height = h;
+    can.ctx = can.getContext("2d");
+    return can;
+  }
+
+  copyToCanvas(image) {
+    const can = document.createElement("canvas");
+    can.width = image.naturalWidth || image.width;
+    can.height = image.naturalHeight || image.height;
+
+    can.ctx = can.getContext("2d");
+    can.ctx.drawImage(image, 0, 0);
+    return can;
+  }
+
+  getChannel(channelName, image) {
+    const copy = this.copyToCanvas(image);
+    const ctx = copy.ctx;
+    ctx.fillStyle = channels[channelName];
+    ctx.globalCompositeOperation = "multiply";
+    ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.globalCompositeOperation = "destination-in";
+    ctx.drawImage(image, 0, 0);
+    ctx.globalCompositeOperation = "source-over";
+    return copy;
+  }
+
+  separatedRGB(image) {
+    return {
+      red: this.getChannel("red", image),
+      green: this.getChannel("green", image),
+      blue: this.getChannel("blue", image),
+    };
+  }
+
+  layerImagesInCanvas(image, RGB, canvas, offset) {
+    const ctx = canvas.ctx;
+
+    ctx.clearRect(0, 0, canvas.height, canvas.width);
+
+    ctx.drawImage(RGB.red, 0, 0);
+    ctx.globalCompositeOperation = "lighter";
+    ctx.drawImage(RGB.green, offset, offset);
+    ctx.drawImage(RGB.blue, -offset, -offset);
+    ctx.globalCompositeOperation = "destination-in";
+    ctx.drawImage(image, 0, 0);
+    ctx.globalCompositeOperation = "source-over";
+  }
+}
